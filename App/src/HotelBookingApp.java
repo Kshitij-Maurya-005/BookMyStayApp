666d9e1ab855d0225b5cbcb1b191a6e36f1ee20f
Kshitij-Maurya-005 <kshitijmaurya005@gmail.com>
2026-03-20 19:47:21 +0530

    Room Search & Availability Check

--- App/src/HotelBookingApp.java
+++ App/src/HotelBookingApp.java
@@ -1,16 +1,6 @@
-/**
- * MAIN CLASS: UseCase3InventorySetup
- *
- * Demonstrates centralized inventory management
- *
- * @version 3.1
- */
-
 public class HotelBookingApp {
 
     public static void main(String[] args) {
-
-        System.out.println("Hotel Room Inventory Status\n");
 
         // Create room objects
         Room single = new SingleRoom();
@@ -20,19 +10,15 @@ public class HotelBookingApp {
         // Initialize inventory
         RoomInventory inventory = new RoomInventory();
 
-        // Display Single Room
-        System.out.println("Single Room:");
-        single.displayRoomDetails();
-        System.out.println("Available Rooms: " + inventory.getAvailability("Single") + "\n");
+        // Create search service
+        RoomSearchService searchService = new RoomSearchService();
 
-        // Display Double Room
-        System.out.println("Double Room:");
-        doubleRoom.displayRoomDetails();
-        System.out.println("Available Rooms: " + inventory.getAvailability("Double") + "\n");
-
-        // Display Suite Room
-        System.out.println("Suite Room:");
-        suite.displayRoomDetails();
-        System.out.println("Available Rooms: " + inventory.getAvailability("Suite"));
+        // Perform search (READ ONLY)
+        searchService.searchAvailableRooms(
+                inventory,
+                single,
+                doubleRoom,
+                suite
+        );
     }
 }
